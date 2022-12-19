6ca2d394df9041b9e0410d619039abdb31187c4a
vivorth <s.vivorth3@gmail.com>
2022-12-19 13:58:53 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-
+const routes = require('./flip_flash_api/routes/flip_flash')
+// set up express app
 const app = express();
 
+app.use(routes)
 
-//listen requests
-
-app.listen(process.env.port || 8000, function() {
-    console.log('App is running!')
+// listen for requests
+app.listen(process.env.port || 8000, function(){
+   console.log('Server is running...');
 });
